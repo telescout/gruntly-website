072cd840a0f19db52efd66fe4ca00847b7be5f37
Sergey Kovalev <sergey@weedydidie.com>
2023-10-26 12:42:29 +0100

    feat: blog added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
         //     position: 'left',
         //     label: 'Tutorial',
         //   },
-        //   { to: '/blog', label: 'Blog', position: 'left' },
+          { to: '/blog', label: 'Blog', position: 'right' },
         //   {
         //     href: 'https://github.com/facebook/docusaurus',
         //     label: 'GitHub',
